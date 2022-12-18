38a959a22ee43ef1d39c2bfd0072af55e99a52f0
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-18 02:51:34 +0200

    JavaScript and CSS added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+div.setAttribute('style', 'padding:16px');
+
+div.textContent = 'Div 1'
+
+container.appendChild(div);
