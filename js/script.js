131dcd867c70f63c642b992d46236b08cb7ddb3b
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-22 07:57:59 +0200

    Added the reset button

--- js/script.js
+++ js/script.js
@@ -1,32 +1,42 @@
 const container = document.getElementById("container");
+const rowItems = document.querySelector('#container');
+const button = document.getElementById('size');
+const resetBtn = document.querySelector('.reset')
+let size =8;
 
-let size;
+function makeRows(size) {
+  container.style.setProperty('--grid-rows', size);
+  container.style.setProperty('--grid-cols', size);
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-
-  for (c = 0; c < (rows * cols); c++) {
+  for (c = 0; c < (size * size); c++) {
     let cell = document.createElement("div");
     cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(2,2);
+makeRows(size);
 
-const rowItems = document.querySelector('#container');
+
 rowItems.addEventListener('mouseover', (box) => {
     if(box.target.matches('.grid-item')){
         box.target.setAttribute('style', 'background-color:black');
     }
 });
 
-const button = document.getElementById('size');
 button.addEventListener('click',() =>{
     size = prompt('Enter the grid size');
-    makeRows(size,size);
+    
+    makeRows(0,0);
+    
 });
+
+function reset(){
+  makeRows(size);
+}
+
+resetBtn.addEventListener('click', reset);
+
 
 
 
